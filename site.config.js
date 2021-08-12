ca630fc4c29bbc58fc4edf8bf66c3d6b9365ba6e
QQ-and-qq <66247217+QQ-and-qq@users.noreply.github.com>
2021-08-12 13:00:55 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
   rootNotionPageId: '3fa424bb468c42ca8cf2f23bef85fe83',
-https://deluxe-router-46e.notion.site/CAD-3fa424bb468c42ca8cf2f23bef85fe83
+
   
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
